80e391a84b7a4d26afeefe2eba1797fc3b464d96
Klajdi Gashi <klajdi.gashi01@gmail.com>
2024-11-11 09:07:16 +0100

    Domain Models added

---
+++ NZWalks/NZWalksAPI/Models/Domain/Region.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Primitives;
+
+namespace NZWalksAPI.Models.Domain
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+
+        public string Code { get; set; }
+        
+        public string Name { get; set; }
+
+        public string? RegionImageUrl { set; get; } //nullable string type, so this can be N/A.
+    }
+}
