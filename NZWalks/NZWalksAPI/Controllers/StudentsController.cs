242e2a7acfd454103a915c786566063a1e9e7ac5
Klajdi Gashi <klajdi.gashi01@gmail.com>
2024-11-11 08:48:28 +0100

    Api Controllers

---
+++ NZWalks/NZWalksAPI/Controllers/StudentsController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Extensions;
+
+namespace NZWalksAPI.Controllers
+{
+    // https://localhost:portnumber/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+
+      private static List<string> students = new List<string> {"John","Jane","Mark","Emily","Joe","Rick"};
+        // GET: https://localhost:portnumber/api/students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[students.Count];
+
+            for (int i = 0; i < students.Count; i++)
+            {
+                studentNames[i] = students[i];
+            }
+
+            return Ok(studentNames);
+        }
+
+        [HttpPost]
+        public IActionResult AddStudent(string student) 
+        {
+            if (string.IsNullOrEmpty(student))
+            {
+                return BadRequest("Cannot Add a Student into the List!");
+            }
+
+            students.Add(student);
+
+            return CreatedAtAction(nameof(AddStudent), student);
+        }
+
+        [HttpDelete("{name}")]
+        public IActionResult DeleteStudent(string name) 
+        {
+            var StudentList = students.FirstOrDefault(w => w.Equals(name, StringComparison.OrdinalIgnoreCase));
+
+            if (StudentList == null)
+            {
+                return NotFound($"Student {name} not found");
+            }
+
+            students.Remove(StudentList);
+
+            return Ok($"Student {students} has been removed successfully");
+
+        }
+
+        public class StudentNameChanger
+        {
+            public string OldName { get; set; }
+
+            public string NewName { get; set; }
+        };
+
+
+        [HttpPut("{name}")]
+
+        public IActionResult ChangeStudentName(string name, [FromBody] string NewName)
+        {
+            var StudentList = students.FirstOrDefault(w => w.Equals(name, StringComparison.OrdinalIgnoreCase));
+            if (string.IsNullOrEmpty(name))
+            {
+                return BadRequest("The new name cannot be empty!");
+            }
+
+            int Index = students.IndexOf(StudentList);
+            students[Index] = NewName;
+
+            return Ok($"Student {name} was changed to {NewName}");
+
+        }
+
+
+    
+    
+    }
+
+}
---
+++ NZWalks/NZWalksAPI/Controllers/WorkController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+
+namespace NZWalksAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkController : ControllerBase
+    {
+        private static List<string> WorkerNames = new List<string> { "Klajd", "Lirak", "Astrit", "Lirim", "Besmir" };
+
+        [HttpGet]
+        public IActionResult GetWorkers()
+        {
+            return Ok(WorkerNames);
+        }
+        
+        [HttpPost]
+        public IActionResult AddWorker(string workerName)
+        {
+
+            if (string.IsNullOrEmpty(workerName))
+            {
+                return BadRequest("Cannot Add a Null name");
+            }
+
+            WorkerNames.Add(workerName);
+
+            return CreatedAtAction(nameof(GetWorkers), new {name = workerName}, workerName);
+            //return Ok($"User {workerName} added into the list!");
+        }
+
+        [HttpDelete("{name}")]
+        public IActionResult DeleteWorker(string name)
+        {
+            var worker = WorkerNames.FirstOrDefault(w => w.Equals(name, StringComparison.OrdinalIgnoreCase));
+
+            if (worker == null)
+            {
+                return NotFound($"Worker {name} not found");
+            }
+
+            WorkerNames.Remove(worker);
+
+            return Ok($"Worker {name} has been removed successfully");
+        }
+
+
+        public class WorkerUpdateModel
+        {
+            public string OldName { get; set; }
+            public string NewName { get; set; }
+        }
+
+
+        [HttpPut("{name}")]
+        public IActionResult UpdateWorkerName(string updatedWorker, string name)
+        {
+            var worker = WorkerNames.FirstOrDefault(w => w.Equals(name, StringComparison.OrdinalIgnoreCase));
+
+            if (worker == null)
+            {
+                return NotFound();
+            }
+
+            int index = WorkerNames.IndexOf(worker);
+            WorkerNames[index] = updatedWorker;
+
+
+            var result = new WorkerUpdateModel()
+            {
+                OldName = name,
+                NewName = updatedWorker
+            };
+            return Ok($"Worker {name} has been modified to {updatedWorker} ");
+        }
+
+    }
+}
