c50bf8e8057a8b814eeb207bf1425b7145ad8013
Klajdi Gashi <klajdi.gashi01@gmail.com>
2024-11-12 10:46:38 +0100

    Added Validations in all the DTOs

--- NZWalks/NZWalksAPI/Models/DTO/UpdateWalkRequestDto.cs
+++ NZWalks/NZWalksAPI/Models/DTO/UpdateWalkRequestDto.cs
@@ -1,8 +1,13 @@
-﻿namespace NZWalksAPI.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NZWalksAPI.Models.DTO
 {
     public class UpdateWalkRequestDto
     {
+        [MaxLength(100, ErrorMessage = "Name has to be a maximum of 100 characters")]
         public string Name { get; set; }
+
+        [MaxLength(200, ErrorMessage = "Description has to be a maximum of 200 characters")]
         public string Description { get; set; }
         public double LengthInKm { get; set; }
         public string? WalkImageUrl { get; set; } //this is a nullable variable as well
