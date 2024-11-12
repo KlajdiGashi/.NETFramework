5369cf0a2feaabfa9a0b2138fb439de1c711d6fe
Klajdi Gashi <klajdi.gashi01@gmail.com>
2024-11-12 10:46:38 +0100

    Added Validations in all the DTOs

--- NZWalks/NZWalksAPI/Models/DTO/AddWalkRequestDto.cs
+++ NZWalks/NZWalksAPI/Models/DTO/AddWalkRequestDto.cs
@@ -4,16 +4,23 @@ namespace NZWalksAPI.Models.DTO
 {
     public class AddWalkRequestDto
     {
+        [Required]
         [MaxLength(100, ErrorMessage = "Name has to be a maximum of 100 characters")]
         public string Name { get; set; }
 
+        [Required]
         [MaxLength(200, ErrorMessage = "Description has to be a maximum of 200 characters")]
         public string Description { get; set; }
+
+        [Required]
         public double LengthInKm { get; set; }
+
         public string? WalkImageUrl { get; set; } //this is a nullable variable as well
 
+        [Required]
         public Guid DifficultyId { get; set; }
 
+        [Required]
         public Guid RegionId { get; set; }
     }
 }
--- NZWalks/NZWalksAPI/Models/DTO/UpdateWalkRequestDto.cs
+++ NZWalks/NZWalksAPI/Models/DTO/UpdateWalkRequestDto.cs
@@ -1,14 +1,23 @@
-﻿namespace NZWalksAPI.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NZWalksAPI.Models.DTO
 {
     public class UpdateWalkRequestDto
     {
+        [Required]
+        [MaxLength(100, ErrorMessage = "Name has to be a maximum of 100 characters")]
         public string Name { get; set; }
+
+        [MaxLength(200, ErrorMessage = "Description has to be a maximum of 200 characters")
         public string Description { get; set; }
+        [Required]
         public double LengthInKm { get; set; }
         public string? WalkImageUrl { get; set; } //this is a nullable variable as well
 
+        [Required]
         public Guid DifficultyId { get; set; }
 
+        [Required]
         public Guid RegionId { get; set; }
     }
 }
