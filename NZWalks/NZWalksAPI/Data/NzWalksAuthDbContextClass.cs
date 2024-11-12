49ad931cc750878e9367149e1b0d17f48697cf4a
Klajdi Gashi <klajdi.gashi01@gmail.com>
2024-11-12 14:51:11 +0100

    Created Roles for Authentication in the Database

---
+++ NZWalks/NZWalksAPI/Data/NzWalksAuthDbContextClass.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NZWalksAPI.Data
+{
+    public class NzWalksAuthDbContext: IdentityDbContext
+    {
+        public NzWalksAuthDbContext(DbContextOptions<NzWalksAuthDbContext> options) :base(options)
+        {
+
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var readerRoleId = "50be801b-2acc-4ccb-87c8-2e513a5ca856";
+            var writerRoleId = "1d82a436-f930-4acb-b562-7ec45f7ebe3f";
+
+
+
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = readerRoleId,
+                    ConcurrencyStamp = readerRoleId,
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper()                    
+                },
+                new IdentityRole
+                {
+                    Id = writerRoleId,
+                    ConcurrencyStamp = writerRoleId,
+                    Name = "Writer",
+                    NormalizedName = "Writer".ToUpper()
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
+    }
+}
--- NZWalks/NZWalksAPI/Program.cs
+++ NZWalks/NZWalksAPI/Program.cs
@@ -18,6 +18,11 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<NZWalksDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalksConnectionString")));
 
+builder.Services.AddDbContext<NzWalksAuthDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalksAuthConnectionString")));
+
+
+
 builder.Services.AddScoped<IRegionRepository, SQLRegionRepository>();
 builder.Services.AddScoped<IWalkRepository, SQLWalkRepository>();
 
