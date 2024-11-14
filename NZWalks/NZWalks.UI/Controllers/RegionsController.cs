7a15b54cb0e4d9ee78e97f97dde4bad6043b7325
Klajdi Gashi <klajdi.gashi01@gmail.com>
2024-11-14 09:15:54 +0100

    Add Method With UI Implementation

--- NZWalks/NZWalks.UI/Controllers/RegionsController.cs
+++ NZWalks/NZWalks.UI/Controllers/RegionsController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.UI.Models;
 using NZWalks.UI.Models.DTO;
+using System.Text.Json;
+using static System.Net.WebRequestMethods;
 
 namespace NZWalks.UI.Controllers
 {
@@ -34,6 +37,42 @@ namespace NZWalks.UI.Controllers
             }
 
             return View(response);
+        }
+
+        [HttpGet]
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Add(AddRegionViewModel model)
+        {
+            var client = httpClientFactory.CreateClient();
+
+            var httpRequestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("https://localhost:7287/api/regions"),
+                Content = new StringContent(JsonSerializer.Serialize(model), System.Text.Encoding.UTF8, "application/json")
+            };
+            var httpResponseMessage = await client.SendAsync(httpRequestMessage);
+            
+            httpResponseMessage.EnsureSuccessStatusCode();
+
+            var response =await httpResponseMessage.Content.ReadFromJsonAsync<RegionDto>();
+
+            if (response is not null)
+            {
+                return RedirectToAction("Index", "Regions");
+            }
+            else
+            {
+                return View();
+            }
+
         }
     }
 }
